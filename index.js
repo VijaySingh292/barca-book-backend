f410423901a6af5179a8eeb4269cf926277b396c
VijaySingh292 <vijaysuperstar292@gmail.com>
2024-05-31 04:22:16 +0530

    changes_01

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const db = new Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect()
--- users.js
+++ users.js
@@ -9,11 +9,7 @@ const { Client } = pkg;
 const router = Router();
 
 const db = new Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 db.connect()
